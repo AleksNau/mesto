41299d87ccbc30e06ed53c0d5be53390fc4fba09
AleksNau <workciti@yandex.ru>
2023-03-04 14:59:07 +0300

    feature/scripts: сделать заполнения полей и обработчик кнопки "сохранить"

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,44 @@
 const popupElement = document.querySelector('.popup');
 const closeButton = popupElement.querySelector('.popup__close-button');
-const editButton = document.querySelector('.profile__edit-button');
-let addButton = document.querySelector('.profile__add-button');
+const saveButton = popupElement.querySelector('.popup__save-button');
+
+const profileElement = document.querySelector('.profile');
+const editButton = profileElement.querySelector('.profile__edit-button');
+const addButton = profileElement.querySelector('.profile__add-button');
+const profileName = profileElement.querySelector('.profile__name');
+const profileInfo = profileElement.querySelector('.profile__info');
+
+let name = popupElement.querySelector('.popup__input_type-name');
+let info = popupElement.querySelector('.popup__input_type-info');
+
+function fillName() {
+    name.value = profileName.innerText;
+}
+
+function fillInfo() {
+    info.value = profileInfo.innerText;
+}
+
 
 const popupOpen = function () {
     popupElement.classList.add('popup_is-opened');
+    fillName();
+    fillInfo();
 }
 
 const popupClose = function () {
     popupElement.classList.remove('popup_is-opened');
+    name.value = undefined;
+    info.value = undefined;
 }
+
+const popupSave = function (event) {
+    event.preventDefault();
+    profileName.innerText = name.value;
+    profileInfo.innerText = info.value;
+    popupElement.classList.remove('popup_is-opened');
+}
+
 
 const popupCloseByClickOnOverlay = function (event) {
     if (event.target !== event.currentTarget){
@@ -21,3 +50,4 @@ const popupCloseByClickOnOverlay = function (event) {
 editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
 popupElement.addEventListener('click',popupCloseByClickOnOverlay);
+saveButton.addEventListener('click',popupSave);
