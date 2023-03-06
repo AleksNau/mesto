0e9cfc1c75e416df32f8aaf608f20f0b42c9713a
AleksNau <workciti@yandex.ru>
2023-03-06 21:57:34 +0300

    fix: убрать лишнее в скриптах, шрифтах и стилях popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 const popupElement = document.querySelector('.popup');
+const popupForm = popupElement.querySelector('.popup__conteiner');
 const closeButton = popupElement.querySelector('.popup__close-button');
 const saveButton = popupElement.querySelector('.popup__save-button');
 
 const profileElement = document.querySelector('.profile');
 const editButton = profileElement.querySelector('.profile__edit-button');
-const addButton = profileElement.querySelector('.profile__add-button');
 const profileName = profileElement.querySelector('.profile__name');
 const profileInfo = profileElement.querySelector('.profile__info');
 
@@ -12,42 +12,29 @@ let name = popupElement.querySelector('.popup__input_type-name');
 let info = popupElement.querySelector('.popup__input_type-info');
 
 function fillName() {
-    name.value = profileName.innerText;
-}
-
-function fillInfo() {
-    info.value = profileInfo.innerText;
+    name.value = profileName.textContent;
+    info.value = profileInfo.textContent;
 }
 
 
 const popupOpen = function () {
-    popupElement.classList.add('popup_is-opened');
+    popupElement.classList.add('popup_opened');
     fillName();
-    fillInfo();
 }
 
 const popupClose = function () {
-    popupElement.classList.remove('popup_is-opened');
-    name.value = undefined;
-    info.value = undefined;
+    popupElement.classList.remove('popup_opened');
 }
 
 const popupSave = function (event) {
     event.preventDefault();
-    profileName.innerText = name.value;
-    profileInfo.innerText = info.value;
-    popupElement.classList.remove('popup_is-opened');
-}
-
-
-const popupCloseByClickOnOverlay = function (event) {
-    if (event.target !== event.currentTarget) {
-        return;
-    }
+    profileName.textContent = name.value;
+    profileInfo.textContent = info.value;
     popupClose();
 }
 
+
 editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
-popupElement.addEventListener('click', popupCloseByClickOnOverlay);
 saveButton.addEventListener('click', popupSave);
+popupForm.addEventListener('submit',popupSave);
