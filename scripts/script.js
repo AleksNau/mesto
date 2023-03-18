a21919c01232a87b9d03d603749e66aff97f0361
AleksNau <workciti@yandex.ru>
2023-03-18 16:55:24 +0300

    feature: сделать заполение страницы через js

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ const editButton = profileElement.querySelector('.profile__edit-button');
 const addButton = profileElement.querySelector('.profile__add-button');
 const profileName = profileElement.querySelector('.profile__name');
 const profileInfo = profileElement.querySelector('.profile__info');
+const elementsList = document.querySelector(".elements");
 
 let name = popupElement.querySelector('.popup__input_type-name');
 let info = popupElement.querySelector('.popup__input_type-info');
@@ -51,3 +52,45 @@ editButton.addEventListener('click', popupOpen);
 closeButton.addEventListener('click', popupClose);
 popupElement.addEventListener('click', popupCloseByClickOnOverlay);
 saveButton.addEventListener('click', popupSave);
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+initialCards.forEach(renderItem);
+
+function renderItem(item) {
+    elementsList.insertAdjacentHTML("beforeend", `
+    <li class="elements__item">
+                <img src="${item.link}" alt="'Элемент'" class="elements__image">
+                <div class="elements__info">
+                    <p class="elements__text">${item.name}</p>
+                    <img src="./images/like.svg" alt="like" class="elements__like">
+                </div>
+            </li>
+    
+    `)
+}
