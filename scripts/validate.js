1809662f51bd70ac888277ccd2ed16179355dcb5
AleksNau <workciti@yandex.ru>
2023-04-26 17:37:49 +0300

    formvalid class

--- scripts/validate.js
+++ scripts/validate.js
@@ -8,84 +8,69 @@ const validationConfig = {
     inputErrorClass: 'popup__input_type_error'
 };
 
-const enableValidation = ({formSelector, ...rest}) => {
-    const forms = Array.from(document.querySelectorAll(formSelector));
-    forms.forEach(form => {
-        form.addEventListener('submit', (evt) => {
-            evt.preventDefault();
-        })
-        setEventListeners(form, rest);
-    })
-}
 
-//накладываем слушатели на форму
-function setEventListeners(formValidate, {inputSelector, submitButtonSelector, inputErrorClass, ...rest}) {
-    //создаём массив из инпутов
-    const formInputs = Array.from(formValidate.querySelectorAll(inputSelector));
-    const formButton = formValidate.querySelector(submitButtonSelector);
-    disableButton(formButton, rest);
-    //каждому инпуту добавляем слушатель
-    formInputs.forEach(input => {
-        input.addEventListener('input', () => {
-            checkInputValidity(input, inputErrorClass);
-            //проверить есть ли хоть один незаполеный инпут
-            if (hasInvalidInput(formInputs)) {
-                disableButton(formButton, rest);
-            } else {
-                enableButton(formButton, rest);
-            }
-        })
-    })
-}
 
-function checkInputValidity(item, errorClass) {
-    //находим сообщение(span) об ошибке называть по принципу id инпута к которому он относится +"-error"
-    const currentInputErrorConteiner = document.querySelector(`#${item.id}-error`);
-    //checkValidity встроенный метод который возвращает тру или фолс основываясь на разметке
-    if (item.checkValidity()) {
-        currentInputErrorConteiner.textContent = "";
-        item.classList.remove(errorClass);
-    } else {
-        //выводим встроенное сообщение validationMessage
-        currentInputErrorConteiner.textContent = item.validationMessage;
-        item.classList.add(errorClass);
-    }
-}
-
-//проверяем вернёт ли какой элемент то что он не заполнен
-function hasInvalidInput(formInputs) {
-    return formInputs.some(item => !item.validity.valid);
-}
-
-function enableButton(button, {inactiveButtonClass}) {
-    button.classList.remove(inactiveButtonClass);
-    button.removeAttribute('disabled', true);
-}
-
-function disableButton(button, {inactiveButtonClass}) {
-    button.classList.add(inactiveButtonClass);
-    button.setAttribute('disabled', true);
-}
-
-enableValidation(validationConfig);
 
 class FormValidator {
     //добавить в параметры конструктора объект с его параметрами
-    constructor(objectSettings, formElementSelector) {
-        this._objectSettings=objectSettings;
-        this._formElementSelector=formElementSelector;
+    constructor(formSelector,{...rest}) {
+        this._form=document.querySelector(formSelector);
+        this._rest = rest;
     }
-    enableValidation () {
-        const forms = Array.from(document.querySelectorAll(this._objectSettings.formSelector));
-        forms.forEach(form => {
-            form.addEventListener('submit', (evt) => {
-                evt.preventDefault();
+    enableValidation = () => {
+        this._form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        
+        this.setEventListeners(this._rest);
+    }
+
+    hasInvalidInput(formInputs) {
+        return formInputs.some(item => !item.validity.valid);
+    }
+
+    setEventListeners({inputSelector, submitButtonSelector, inputErrorClass, ...rest}) {
+        //создаём массив из инпутов
+        const formInputs = Array.from(this._form.querySelectorAll(inputSelector));
+        const formButton = this._form.querySelector(submitButtonSelector);
+        this.disableButton(formButton, rest);
+        //каждому инпуту добавляем слушатель
+        formInputs.forEach(input => {
+            input.addEventListener('input', () => {
+                this.checkInputValidity(input, inputErrorClass);
+                //проверить есть ли хоть один незаполеный инпут
+                if (this.hasInvalidInput(formInputs)) {
+                    this.disableButton(formButton, rest);
+                } else {
+                    this.enableButton(formButton, rest);
+                }
             })
-            console.log(form);
-            setEventListeners(form, rest);
         })
+    }
+    checkInputValidity(item, errorClass) {
+        //находим сообщение(span) об ошибке называть по принципу id инпута к которому он относится +"-error"
+        const currentInputErrorConteiner = document.querySelector(`#${item.id}-error`);
+        //checkValidity встроенный метод который возвращает тру или фолс основываясь на разметке
+        if (item.checkValidity()) {
+            currentInputErrorConteiner.textContent = "";
+            item.classList.remove(errorClass);
+        } else {
+            //выводим встроенное сообщение validationMessage
+            currentInputErrorConteiner.textContent = item.validationMessage;
+            item.classList.add(errorClass);
+        }
+    }
+
+    enableButton(button, {inactiveButtonClass}) {
+        button.classList.remove(inactiveButtonClass);
+        button.removeAttribute('disabled', true);
+    }
+    
+    disableButton(button, {inactiveButtonClass}) {
+        button.classList.add(inactiveButtonClass);
+        button.setAttribute('disabled', true);
     }
 }
 
-const fv = new FormValidator(validationConfig);
+const fv = new FormValidator('.popup__form_profile',validationConfig);
 fv.enableValidation();
