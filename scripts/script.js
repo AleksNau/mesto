13e3b31c8e89a216fd8549364fb84c364b8d621b
AleksNau <workciti@yandex.ru>
2023-04-28 14:13:39 +0300

    add class file Card

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 import FormValidator from './FormValidator.js'
+import Card from './Card.js'
 //переменные профиля
 const profileElement = document.querySelector('.profile');
 const buttonEdit = profileElement.querySelector('.profile__edit-button');
@@ -134,52 +135,7 @@ popupCloseByClickOnOverlay();
 
 //Создаем класс Card
 
-class Card {
-    //принимает в себя два параметра
-    constructor(templateSelector, data) {
-        this._data = data;
-        this._template = templateSelector;
-    }
 
-//метод навешивания лайка
-    #addLike(event) {
-        event.currentTarget.classList.toggle("elements__like_active");
-    }
-
-//метод удаления карточки
-    #deleteCard = () => {
-        this.element.remove();
-    }
-
-    #openImage(event) {
-        openPopup(popupImage);
-        zoomCardImage(event);
-        addListenerCloseByEsc(popupImage);
-    }
-
-//Метод навешивания всех слушателей
-    setEventListener() {
-        //закрасить лайк
-        this.element.querySelector(".elements__like").addEventListener("click", this.#addLike);
-        //просмотреть изображение полностью
-        this.element.querySelector(".elements__image").addEventListener("click", this.#openImage);
-        //функция удаления
-        this.element.querySelector(".elements__delete").addEventListener("click", this.#deleteCard);
-    }
-
-//Основной (публичный) метод создания карточки, в котором мы вызываем нужные методы и наполняем будущую карточку данными из формы.
-    createCard() {
-        this.element = this._template.cloneNode(true).children[0];
-        const imageElement = this.element.querySelector(".elements__image");
-        imageElement.src = this._data.link;
-        imageElement.alt = this._data.name;
-        this.element.querySelector(".elements__text").textContent = this._data.name;
-        this.setEventListener();
-
-        return this.element;
-    }
-
-}
 
 //добавить карточку на страницу
 function addCard(item) {
