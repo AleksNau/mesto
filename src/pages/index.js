e3b4476a811627139e0505eb275f4784348dc09c
AleksNau <workciti@yandex.ru>
2023-05-12 22:01:03 +0300

    рабочий webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import {initialCards, validationConfig} from './constants.js'
-import FormValidator from './FormValidator.js'
-import Card from './Card.js'
-import UserInfo from './UserInfo.js'
-import PopupWithForm from "./PopupWithForm.js"
-import Section from './Section.js'
-import PopupWithImage from './PopupWithImage.js'
+import './index.css'
+import {initialCards, validationConfig} from '../components/constants.js'
+import FormValidator from '../components/FormValidator.js'
+import Card from '../components/Card.js'
+import UserInfo from '../components/UserInfo.js'
+import PopupWithForm from "../components/PopupWithForm.js"
+import Section from '../components/Section.js'
+import PopupWithImage from '../components/PopupWithImage.js'
 //переменные профиля
 const profileElement = document.querySelector('.profile');
 const buttonEdit = profileElement.querySelector('.profile__edit-button');
