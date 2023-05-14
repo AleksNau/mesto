547411c67be6e74c4c1ef8077a531d8f05515c6f
AleksNau <workciti@yandex.ru>
2023-05-14 11:22:24 +0300

    сделть метод супер

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,8 +2,7 @@ import Popup from "./Popup.js";
 
 export default class PopupWithForm extends Popup {
     constructor(popupSelector, submit) {
-        super();
-        this._popup = document.querySelector(popupSelector);
+        super(popupSelector);
         this._submit = submit;
         this._form = this._popup.querySelector(".popup__form");
     }
