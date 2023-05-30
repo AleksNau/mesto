840b9585f065a7317608992c90207d43ac0bc40a
AleksNau <workciti@yandex.ru>
2023-05-30 17:28:31 +0300

    удалить лишний класс

--- src/components/PopupAvatar.js
+++
@@ -1,7 +0,0 @@
-import PopupWithForm from "./PopupWithForm.js";
-
-class PopupAvatar extends PopupWithForm {
-    constructor(popupSelector, submit) {
-        super(popupSelector,submit);
-    }
-}
